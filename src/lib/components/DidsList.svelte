faef3e501190afc0856f08e912816b377dda7ec0
suibari <404.not.found.address@gmail.com>
2025-02-10 01:45:28 +0900

    DidListsの見た目修正

--- src/lib/components/DidsList.svelte
+++ src/lib/components/DidsList.svelte
@@ -4,14 +4,14 @@
 
 <div class="flex-col lg:ml-2">
   <p class="font-semibold">Drawers:</p>
-  <ul class="flex flex-col gap-2">
+  <ul class="flex flex-col gap-2 max-h-60 overflow-y-auto">
     {#each dids as did}
       <li>
         <a
           href={`https://bsky.app/profile/${did}`}
           target="_blank"
           rel="noopener noreferrer"
-          class="block px-4 py-2 bg-gray-200 rounded-lg text-gray-800 hover:bg-gray-300 transition"
+          class="block px-4 py-1 bg-gray-200 rounded-lg text-gray-800 hover:bg-gray-300 transition text-sm"
         >
           {did}
         </a>
